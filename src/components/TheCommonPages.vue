9831b7797de9d1f07d85b6f5d62588aea36d9691
gxh <2036902768@qq.com>
2026-04-23 12:17:25 +0800

    feat: 添加公共页面的提示组件

--- src/components/TheCommonPages.vue
+++ src/components/TheCommonPages.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <template>
     <hy-config-provider>
         <slot></slot>
+
+        <hy-toast></hy-toast>
+        <hy-modal></hy-modal>
     </hy-config-provider>
 </template>
+
+<style lang="scss" scoped></style>
